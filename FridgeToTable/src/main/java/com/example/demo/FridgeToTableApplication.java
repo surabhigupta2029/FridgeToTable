51569e6982716eedce8edc695a8352d2b8fcf461
nanarboursal <nanar.boursalian01@gmail.com>
2021-03-13 18:04:14 -0800

    created a spring boot project and added dependencies

---
+++ FridgeToTable/src/main/java/com/example/demo/FridgeToTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeToTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeToTableApplication.class, args);
+	}
+
+}
