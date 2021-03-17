3f493fab94bf4481736572fbaa2f5af61a42dc8c
Surabhi Gupta <surabhi2029@gmail.com>
2021-03-16 19:19:34 -0700

    Fixes MySQL setup & print line to confirm run

--- FridgeToTable/src/main/java/com/example/demo/FridgeToTableApplication.java
+++ FridgeToTable/src/main/java/com/example/demo/FridgeToTableApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FridgeToTableApplication {
-
+	
+	public FridgeToTableApplication() {
+		System.out.println("WORKINGG!");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FridgeToTableApplication.class, args);
 	}
